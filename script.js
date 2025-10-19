4b43e15d0ab9fa081c4d44913b6d0556b42c5bd3
mini coder <zubamughal5678@gmail.com>
2025-10-19 21:14:08 +0500

    initil commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function calculateAge(){
+const dobinput= document.getElementById('dob').ariaValueMax;
+if(!dobinput){
+
+}
+const birthdate=new Date(dobinput);
+const today=new Date();
+let years=today.getFullYear()-birthdate.getFullYear();
+let months=today.getMonth()-birthdate.getMonth();
+let days= today.getDate()-birthdate.getDate();
+if(days<0){
+    months--;
+    const prev=new Date(today.getFullYear(),today.getMonth(),0);
+    days+=prev.getDate();
+}
+if(months<0){
+    years--;
+    months+=12;
+}
+document.getElementById('result').textContent=`You are ${years} years, ${months} months , ${days} old `;
+}
